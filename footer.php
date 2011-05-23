d21de1c855b39f4faffd73ea7ae8e4b7378c79f8
www-data <www-data@li230-238.members.linode.com>
2011-05-23 15:44:07 -0400

    Added credits to footer.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 <div id="footer">
 	<p class="copyright">&copy; <a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y') ?></p>
-	<p class="credits"><a href="http://icondock.com">Icons</a> &amp; <a href="http://www.ndesign-studio.com/wp-themes"><?php _e('Wordpress Theme','notepad-theme'); ?></a> <?php _e('by','notepad-theme'); ?> <?php _e('<a href="http://www.ndesign-studio.com">N.Design</a>','notepad-theme'); ?></p>
+	<p class="credits"><a href="https://github.com/ghempton/CodePad">CodePad</a> by <a href="http://codebrief.com">Gordon L. Hempton</a>. Based on <a href="http://www.ndesign-studio.com/wp-themes/notepad">Notepad</a> by <?php _e('<a href="http://www.ndesign-studio.com">N.Design</a>','notepad-theme'); ?>.</p>
 </div>
 <!--/footer -->
 
